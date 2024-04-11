fff40fcdff1ca034780f9668692f3bf60c931786
dcarriel-efor <166526354+dcarriel-efor@users.noreply.github.com>
2024-04-11 15:45:37 +0200

    Update streamlit-app.py
    Rajout Tableau Filtres

--- streamlit-app.py
+++ streamlit-app.py
@@ -31,7 +31,33 @@ def main():
         if os.path.isfile("pubMedResults.csv"):
             result = pd.read_csv("pubMedResults.csv", encoding="utf8").applymap(lambda x: x.decode('utf-8', 'replace') if isinstance(x, bytes) else x)
             st.header("Articles found :")
-            st.dataframe(result, hide_index=True)
+            #st.dataframe(result, hide_index=True)
+            df = result
+            # Sidebar for selecting columns and filtering values
+            st.sidebar.title('Filter Data')
+            selected_columns = st.sidebar.multiselect('Select Columns', df.columns)
+            filters = {}
+            for column in selected_columns:
+                filters[column] = st.sidebar.multiselect(f'Select {column}', df[column].unique())
+
+            # Apply filters
+            filtered_df = df
+            for column, values in filters.items():
+                if values:
+                    filtered_df = filtered_df[filtered_df[column].isin(values)]
+
+            # Display filtered data
+            st.write('Filtered Data:', filtered_df)
+
+            # Export filtered data
+            if st.button('Export Filtered Data'):
+                csv = filtered_df.to_csv(index=False)
+                st.download_button(
+                    label='Download CSV',
+                    data=csv,
+                    file_name='filtered_data.csv',
+                    mime='text/csv'
+                )
         else:
             st.header("Seams an error occured")
 
